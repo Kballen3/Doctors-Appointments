5c3611c14d94b0767b6af1d84b91a75ec193516f
Kaleb Allen <7kaleb29allen@gmail.com>
2019-02-26 14:50:07 -0700

    Doctor Controller

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,50 @@
+class PatientsController < ApplicationController
+  before_action :set_patient only: [:show, :update, :edit, :destroy]
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      redirect_to @patient
+    else 
+      render :new
+    end
+  end
+
+  def update
+    if @patient.update(patient_params)
+      redirect_to patients_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    
+  end
+
+  def destroy
+    @patient.destroy
+    redirect_to patients_path
+  end
+
+  private
+
+    def set_patient
+      @patient.find(params[:id])
+    end
+
+    def patient_params
+      params.require(:patient).permit(:first_name, :last_name)
+    end
+
+end
